b9b19c8520945caaa36b02ea6e79a13a5e356021
DanielRivasMD <danielrivasmd@gmail.com>
2021-01-27 17:25:29 +0100

    add completion subcmd & hide

---
+++ cmd/completion.go
@@ -0,0 +1,88 @@
+/*
+Copyright © 2021 Daniel Rivas <danielrivasmd@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// completionCmd represents the completion command
+var completionCmd = &cobra.Command{
+	Use:    "completion [bash|zsh|fish|powershell]",
+	Hidden: true,
+	Short:  "Generate completion script",
+	Long: `To load completions:
+
+Bash:
+
+$ source <(Bender completion bash)
+
+# To load completions for each session, execute once:
+Linux:
+  $ Bender completion bash > /etc/bash_completion.d/Bender
+MacOS:
+  $ Bender completion bash > /usr/local/etc/bash_completion.d/Bender
+
+Zsh:
+
+# If shell completion is not already enabled in your environment you will need
+# to enable it.  You can execute the following once:
+
+$ echo "autoload -U compinit; compinit" >> ~/.zshrc
+
+# To load completions for each session, execute once:
+$ Bender completion zsh > "${fpath[1]}/_Bender"
+
+# You will need to start a new shell for this setup to take effect.
+
+Fish:
+
+$ Bender completion fish | source
+
+# To load completions for each session, execute once:
+$ Bender completion fish > ~/.config/fish/completions/Bender.fish
+
+Powershell:
+
+PS> Bender completion powershell | Out-String | Invoke-Expression
+
+# To load completions for every new session, run:
+PS> Bender completion powershell > Bender.ps1
+# and source this file from your powershell profile.
+`,
+	DisableFlagsInUseLine: true,
+	ValidArgs:             []string{"bash", "zsh", "fish", "powershell"},
+	Args:                  cobra.ExactValidArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		switch args[0] {
+		case "bash":
+			cmd.Root().GenBashCompletion(os.Stdout)
+		case "zsh":
+			cmd.Root().GenZshCompletion(os.Stdout)
+		case "fish":
+			cmd.Root().GenFishCompletion(os.Stdout, true)
+		case "powershell":
+			cmd.Root().GenPowerShellCompletion(os.Stdout)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(completionCmd)
+}
