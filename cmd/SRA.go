f338b05a44bd93ac51d301e1578a35409c12a734
DanielRivasMD <danielrivasmd@gmail.com>
2021-01-29 17:29:23 +0100

    sra scaffold

---
+++ cmd/SRA.go
@@ -0,0 +1,46 @@
+/*
+Copyright © 2021 Daniel Rivas <danielrivasmd@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// SRACmd represents the SRA command
+var SRACmd = &cobra.Command{
+	Use:   "SRA",
+	Short: "Handles SRA operations",
+	Long: `Daniel Rivas <danielrivasmd@gmail.com>
+
+SRA fetch collects files from Short Read Archive (SRA)
+and checks the state of the downloads
+
+SRA reconstruct binary SRA files to fastq`,
+	Example: `
+Bender SRA help`,
+}
+
+func init() {
+	rootCmd.AddCommand(SRACmd)
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	// flags
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+
+}
