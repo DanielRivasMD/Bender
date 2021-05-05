79ff80631b10f96016d247d48bba94e2763da743
DanielRivasMD <danielrivasmd@gmail.com>
2021-05-05 14:25:22 +0200

    patch diamond run. annotations, database boolean & memory management

--- sh/GenomeDiamond.sh
+++ sh/GenomeDiamond.sh
@@ -29,17 +29,17 @@ fi
 ################################################################################
 
 # make database from syncytin protein sequence
-if [[ ! -f ${libraryDir}/${library} ]]
+if [[ ! -f ${libraryDir}/${library}.dmnd ]]
 then
-  echo "Build BLAST database => ${species}"
-  diamond makedb -in ${libraryDir}/${library}.fasta -d ${libraryDir}/${library}
+  echo "Build Diamond database => ${species}"
+  diamond makedb --in ${libraryDir}/${library}.fasta --db ${libraryDir}/${library}.dmnd
 fi
 
 ################################################################################
 
 # use assembly as query with six reading frames
-echo "Blasting database..."
-gzip -dc ${assemblyDir}/${assembly} | diamond blastx -d ${libraryDir}/${library} -q - -o ${dbDir}/${species}.tsv
+echo "Searching diamond database..."
+gzip --decompress --stdout ${assemblyDir}/${assembly}.fasta.gz | diamond blastx --db ${libraryDir}/${library}.dmnd --query - --frameshift 15 --block-size 5 --index-chunks 1 --out ${dbDir}/${species}.tsv
 
 ################################################################################
 
