9d0e62ba6373ac2396e76a5de2acb6ed137f70c4
DanielRivasMD <danielrivasmd@gmail.com>
2021-10-21 15:47:57 +0200

    update subcmd assembly positions
    import main program logic
    add flags sequence identity & length
    deprecate define file out function

--- cmd/Positions.go
+++ cmd/Positions.go
@@ -17,11 +17,25 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 package cmd
 
 import (
-	"fmt"
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var (
+	identity float64
+	length   float64
+)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// TODO: documentation assembly positions
 // PositionsCmd represents the Positions command
 var PositionsCmd = &cobra.Command{
 	Use:   "Positions",
@@ -32,21 +46,119 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Positions called")
+
+		// execute logic
+		genomicPositionsCollect(inDir + "/" + species)
+
 	},
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 func init() {
 	AssemblyCmd.AddCommand(PositionsCmd)
 
-	// Here you will define your flags and configuration settings.
+	////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// PositionsCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// flags
+	PositionsCmd.Flags().Float64VarP(&identity, "identity", "i", 80, "Percentage of sequence identity")
+	PositionsCmd.Flags().Float64VarP(&length, "length", "l", 400, "Length of sequence alignment")
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// PositionsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// read file, filter records & write
+func genomicPositionsCollect(readFile string) {
+
+	// open an input file, exit on error
+	inputFile, readErr := os.Open(readFile)
+	if readErr != nil {
+		log.Fatal("Error opening input file : ", readErr)
+	}
+
+	// check whether file exists to avoid appending
+	if fileExist(outFile) {
+		os.Remove(outFile)
+	}
+
+	// headers := []string{
+	// 	"qseqid",
+	// 	"sseqid",
+	// 	"pident",
+	// 	"length",
+	// 	"mismatch",
+	// 	"gapopen",
+	// 	"qstart",
+	// 	"qend",
+	// 	"sstart",
+	// 	"send",
+	// 	"evalue",
+	// 	"bitscore",
+	// }
+
+	// scanner.Scan() advances to the next token returning false if an error was encountered
+	scanner := bufio.NewScanner(inputFile)
+
+	for scanner.Scan() {
+
+		// tab separated records
+		records := strings.Split(scanner.Text(), "\t")
+
+		// collect patterns
+		pIdent, _ := strconv.ParseFloat(records[2], 64)
+		alignLen, _ := strconv.ParseFloat(records[3], 64)
+
+		// filter criteria
+		if pIdent >= identity && alignLen >= length {
+			// write
+			writeGenomicPositions(outDir+"/"+species, records)
+		}
+
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// write positions
+func writeGenomicPositions(outFile string, records []string) {
+
+	// declare io
+	f, err := os.OpenFile(outFile, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0666)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer f.Close()
+
+	// declare writer
+	w := bufio.NewWriter(f)
+
+	// writing
+	_, err = w.WriteString(
+		records[0] + " " +
+			records[1] + " " +
+			records[2] + " " +
+			records[6] + " " +
+			records[7] + " " +
+			records[10] +
+			"\n")
+	if err != nil {
+		panic(err)
+	}
+
+	// flush writer
+	w.Flush()
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
